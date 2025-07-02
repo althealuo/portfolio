fa37dd8b302f155e3f73a47a6c228b3279ce3d63
Yingkang Luo <50715127+althealuo@users.noreply.github.com>
2025-07-01 20:52:09 -0400

    better styling

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,10 +1,17 @@
 // src/components/SideNav.jsx
 import "../styles/sidenav.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
 function SideNav() {
   return (
     <nav className="side-nav">
       <ul>
-        <li><a href="#top"><span>&uarr;</span></a></li>
+        <li className="arrow-icon">
+          <a href="#top" title="Back to top">
+            <FontAwesomeIcon icon={faArrowUp} />
+          </a>
+        </li>
         <li><a href="#featured">featured</a></li>
         <li><a href="#mind">mind</a></li>
         <li><a href="#arts">arts</a></li>
