834dd8e4225f2e75fdd686b965cea1526760eaea
Yingkang Luo <50715127+althealuo@users.noreply.github.com>
2025-07-04 22:08:40 -0400

    about page design update, adding about animation

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -4,40 +4,43 @@ import Divider from "../components/Divider";
 
 function About() {
 
-  const story = `I’ve always liked psychology and art, and wanted to explore them through tech. 
-I started with HCI and worked as a software engineer. On the side, I learned VR and enjoyed the possibility of immersive expression.
-Now I'm back in school, researching in CogAI and NeuroAI.
--
+  const story = `and wanted to explore them through tech. I started with HCI and worked as a software engineer. On the side, I learned VR and enjoyed the possibility of immersive expression. Now I'm back in school, researching in CogAI and NeuroAI.
+
 
 In my free hours, I mostly zoom out, sometimes play the piano, paint and take photos. 
 `;
   return (
     <section className="about-section">
-      <div className="about-content">
-        <div className="about-image">
-          <img src="/img/portrait.jpg" alt="Althea Portrait" className="portrait-img" />
-          <p className="portrait-caption MainColorSubtext">My Portrait by Yuke</p>
-        </div>
+  <div className="about-container">
+    <div className="about-image">
+      <img src="/img/portrait-1.png" alt="Althea Portrait" className="portrait-img" />
+      <p className="portrait-caption MainColorSubtext">My Portrait by Yuke</p>
+    </div>
 
-        <div className="about-text MainColorText">
-
+    <div className="about-text">
+      <div className="section-block story-block">
+        <p className="section-label">my story</p>
+        <h2>I’ve always liked psychology and art,</h2>
           {story.split("\n").map((line, i) => (
             <p key={i}>
               {line}
             </p>
           ))}
-          <Divider />
-          
-          <p className="title">
-            current thoughts
-          </p>
-          <ul>
-            <li>Modeling decision-making processes using ML approaches</li>
-            <li>Creating adaptive immersive VR worlds based on human data</li>
-          </ul>
-        </div>
       </div>
-    </section>
+
+
+      <div className="section-block thoughts-block">
+        <p className="section-label">research</p>
+        <h2>My current thoughts are on</h2>
+        <ul>
+          <li>Modeling decision-making processes using ML approaches</li>
+          <li>Creating adaptive immersive VR worlds based on human data</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
+
   );
 }
 
