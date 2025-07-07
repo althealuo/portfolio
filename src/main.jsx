9ce6db2d84be2d1b600ec94345c0c73d1301d44b
Yingkang Luo <50715127+althealuo@users.noreply.github.com>
2025-07-06 22:45:43 -0400

    Revert "added smart router"
    This reverts commit b31452173e5cc796f0e780fb78021aff1730c1a9.

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {
-  BrowserRouter,
-  HashRouter,
-} from "react-router-dom";
-
-const isProd = import.meta.env.PROD;
-const Router = isProd ? HashRouter : BrowserRouter;
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename="/portfolio">
+    <BrowserRouter basename="/portfolio">
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
